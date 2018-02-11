05ec015c6e0b41622008fb59d94fb5a9a9a70a44
oykubozkir <oykubozkir@gmail.com>
2018-02-11 19:45:46 +0300

    ASDASDASD

--- db/index.js
+++ db/index.js
@@ -1,16 +1,45 @@
-const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+const { Pool } = require('pg')
 
-client.connect();
+const pool = new Pool()
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
+module.exports = {
+  query: (text, params, callback) {
+    const start = Date.now()
+    return pool.query(text, params, (err, res) => {
+      const duration = Date.now() - start
+      console.log('executed query', { text, duration, rows: res.rowCount })
+      callback(err, res)
+    })
+  },
+  getClient: (callback) {
+    pool.connect((err, client, done) => {
+      const query = client.query.bind(client)
+
+      // monkey patch the query method to keep track of the last query executed
+      client.query = () => {
+        client.lastQuery = arguments
+        client.query.apply(client, arguments)
+      }
+
+      // set a timeout of 5 seconds, after which we will log this client's last query
+      const timeout = setTimeout(() => {
+        console.error('A client has been checked out for more than 5 seconds!')
+        console.error(`The last executed query on this client was: ${client.lastQuery}`)
+      }, 5000)
+
+      const release = (err) => {
+        // call the actual 'done' method, returning this client to the pool
+        done(err)
+
+        // clear our timeout
+        clearTimeout(timeout)
+
+        // set the query method back to its old un-monkey-patched version
+        client.query = query
+      }
+
+      callback(err, client, done)
+    })
   }
-  client.end();
-});
+}
